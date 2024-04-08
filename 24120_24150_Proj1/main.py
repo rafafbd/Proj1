7f838a978bea415c006d627f9bb21e2d79b1f33d
Rafael Fazion B. Dias <rafafazion9@gmail.com>
2024-04-08 09:24:39 -0300

    Estrutura da class Obra e seletor de opções
    Fiz a estrutura da classe Obra ( escrevi o init e os outros métodos ) e fiz a estrutura do main (seletor de opções e pa)

--- 24120_24150_Proj1/main.py
+++ 24120_24150_Proj1/main.py
@@ -1,2 +1,40 @@
-# class Obras
-# 
+
+import os
+import mat
+import obras
+
+def fazer(escolha):
+    opcoesPrevistas =  "01234"
+    if escolha not in opcoesPrevistas:
+        print("Opção inválida")
+        tecla = input("Pressione [enter] para continuar: ")
+    match escolha:
+        case "0":
+            print("Programa finalizado. Obrigado pelo uso!")
+        case "1":
+            
+            tecla = input("Pressione [enter] para continuar: ")
+        case "2":
+
+            tecla = input("Pressione [enter] para continuar: ")
+        case "3":
+
+            tecla = input("Pressione [enter] para continuar: ")
+        case "4":
+
+            tecla = input("Pressione [enter] para continuar: ")
+    
+
+def SeletorDeOpcoes():
+    opcao = ''
+    while opcao != '0':
+        os.system("cls") or None
+        print("Seletor de opções\n")
+        print("0 - Terminar programa")
+        print("1 - Cadastro de obras de arte")
+        print("2 - Listagem de obras de arte")
+        print("3 - Página web de obras de arte")
+        print("4 - Triângulo de Pascal")
+
+if __name__ == '__main__':
+    SeletorDeOpcoes()
