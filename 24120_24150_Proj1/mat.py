e21dee7933bc4f51ff54d1f5eb4115d43de0170e
RAFAEL FAZION BALDIN DIAS <u24150@academico.cotuca.unicamp.br>
2024-04-10 09:29:59 -0300

    Mudei lagumas coisas da classe Matematica e comcei  a escrever o método triângulo de Pascal

--- 24120_24150_Proj1/mat.py
+++ 24120_24150_Proj1/mat.py
@@ -1,18 +1,39 @@
 class Matematica:
 
     def __init__(self):
-        self.__numeroBase = int
+        self._numeroBase = 0
 
-    def fatorial (x :int) -> int:
-        _produtorio = 2
-        _valor = 1
-        while _produtorio <= x:
-            _valor *= _produtorio
-            _produtorio += 1
+    def fatorial (self, x :int) -> int:
+        produtorio = 2
+        valor = 1
+        while produtorio <= x:
+            valor *= produtorio
+            produtorio += 1
 
-        return _valor
+        return valor
     
 
     def trianguloDePascal(self):
-        _k = 0
-        _n = 0
+        triangulo = []
+        num = 0
+        k = 0
+        n = 0
+        while n <= self._numeroBase:
+            linha = ''
+            while k <= n:
+                num = Matematica.fatorial(n) / Matematica.fatorial(k) * Matematica.fatorial(n-k)
+                k += 1
+                n += 1
+                num = str(num)
+                linha += num
+            linha = linha.ljust(6, " ")
+            triangulo.append(linha)
+        return triangulo
+
+
+mat = Matematica()
+mat._numeroBase = 3
+print(mat.trianguloDePascal())
+
+
+        
