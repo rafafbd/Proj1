80049c0259327773ac0db69fd1761b45a0139697
BryanFukugauchi <bry4n2008@gmail.com>
2024-04-10 11:57:17 -0300

    Adicionado no obras
    Adicionado coisas á classe obras

--- 24120_24150_Proj1/obras.py
+++ 24120_24150_Proj1/obras.py
@@ -7,11 +7,26 @@ class Obra:
         self.Estilo: str[15] = ''
         self.ValorEstimado = 0.0
         self.urlFoto: str[100] = ''
+        self._arqParaGravacao : bool = paraGravacao
+        self._arquivo = open(nomeArq, "a" if paraGravacao else "")
 
-    def lerCamposDoArquivo(self):
-        pass
-    def gravarCamposNoArquivo(self):
-        pass
+    def lerCamposDoArquivo(self, paraGravacao):
+        if not paraGravacao:
+            self._arquivo.readline()
+            linha: str = self._arquivo.readline()
+            self.AnoDaObra = linha[0:4]
+            self.MesDaobra = linha[4:6]
+            self.AutorDaObra = linha[6:100]
+            self.NomeDaObra = linha[100:200]
+            self.Estilo = linha[200:205]
+            self.ValorEstimado = linha[205:215]
+            self.urlFoto = linha[215:]
+
+    def gravarCamposNoArquivo(self, paraGravacao):
+        if paraGravacao:
+            linha = self.AnoDaObra + self.AutorDaObra + self.Estilo + self.MesDaobra + self.NomeDaObra + str(self.ValorEstimado) + self.urlFoto
+            self._arquivo.write(linha)
+            
     def preencherCampos(self, novoAno, novoMes, novoAutor, novoNome, novoEstilo, novoValor, novaURL):
         pass
     def fecharArquivo(self):
